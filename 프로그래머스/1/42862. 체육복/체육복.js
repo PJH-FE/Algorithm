b9676b96770e300331c4181b703b5fc3b7a56ca2
JunHo <68776322+PJH-FE@users.noreply.github.com>
2024-08-22 09:42:39 +0900

    [level 1] Title: 체육복, Time: 0.38 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,10 +1,14 @@
 function solution(n, lost, reserve) {
-    const noReserve = lost.filter(num => reserve.includes(num) === true).length;
-    const filteredLost = lost.filter(num => reserve.includes(num) === false).sort((a,b) => a - b);
-    const filteredReserve = reserve.filter(num => lost.includes(num) === false).sort((a,b) => a - b);
+    // 도난 당했지만, 여벌있는 학생 수    
+    const noReserve = lost.filter(num => reserve.includes(num)).length;
     
+    // 여벌없는 ㄹㅇ 급한 도난당한 학생들
+    const filteredLost = lost.filter(num => !reserve.includes(num)).sort((a,b) => a - b);
     
-    var answer = n - lost.length + noReserve;
+    // 여벌있는 금수저들
+    const filteredReserve = reserve.filter(num => !lost.includes(num)).sort((a,b) => a - b);
+    
+    let answer = n - lost.length + noReserve;
     
     filteredReserve.forEach((num)=>{
         for( let i = 0; i < filteredLost.length; i++ )    {
